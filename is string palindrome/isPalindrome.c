28ac14a3fa105d9c0414963e488ce181697a6a1d
randyajackson <rajackson.dev@gmail.com>
2019-07-24 11:28:49 -0600

    7/24/19 11:30p

---
+++ is string palindrome/isPalindrome.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(void)
+{
+    //the last position in the string is fixed for the null character \0
+
+    char str[80],rev[80];
+
+    printf("Enter a string to check for palindrome: ");
+    scanf("%s", &str);
+    printf("\n");
+
+    int strLength = 0;
+    int i, j = 0;
+
+    for(i = 0; str[i] != '\0'; i++)
+        strLength++;
+
+    for(i = strLength - 1, j = 0; i >= 0; i--, j++)
+    {
+        rev[j] = str[i];
+    }
+        
+    rev[strLength] = '\0';    
+
+    if(strcmp(str, rev) == 0)
+        printf("The string is a palindrome.");
+    else
+        printf("The string is not a palindrome.");
+    
+    scanf("%u");
+
+   return 0;
+}
