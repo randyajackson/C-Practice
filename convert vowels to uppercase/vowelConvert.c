7bf09191f64ede5d92ce132377d03cc9d112af33
randyajackson <rajackson.dev@gmail.com>
2019-07-24 14:31:41 -0600

    7/24/19 2:30p

--- convert vowels to uppercase/vowelConvert.c
+++ convert vowels to uppercase/vowelConvert.c
@@ -1,47 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 
 int main(void)
 {
     //the last position in the string is fixed for the null character \0
    char str[150];
+   char newChar;
 
    printf("Enter a sentence to uppercase vowels: ");
    gets(str); // need to use gets for inputs that have spaces
-
-   int tempChar;
    
    for(int i = 0; i < strlen(str); i++)
    {
        if(
-           (char)str[i] == 'a' ||
-           (char)str[i] == 'e' ||
-           (char)str[i] == 'i' ||
-           (char)str[i] == 'o' ||
-           (char)str[i] == 'u' 
+           str[i] == 'a' ||
+           str[i] == 'e' ||
+           str[i] == 'i' ||
+           str[i] == 'o' ||
+           str[i] == 'u' 
        )
        {
-           tempChar = (int)str[i];
-           tempChar = toupper(tempChar);
-
-           str[i] = (char)tempChar;
+           
+           str[i] = str[i] - 32;
        }
-
-       if(
-           (char)str[i] == 'A' ||
-           (char)str[i] == 'E' ||
-           (char)str[i] == 'I' ||
-           (char)str[i] == 'O' ||
-           (char)str[i] == 'U' 
+       else if(
+           str[i] == 'A' ||
+           str[i] == 'E' ||
+           str[i] == 'I' ||
+           str[i] == 'O' ||
+           str[i] == 'U' 
        )
        {
-           tempChar = (int)str[i];
-           tempChar = tolower(tempChar);
-
-           str[i] = (char)tempChar;
+            str[i] = str[i] + 32;     
        }
+       else{}     
+       
    }
 
    printf("%s", str);        
