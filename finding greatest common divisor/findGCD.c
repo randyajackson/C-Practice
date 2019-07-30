18d6d503e2c020278ccc092457b98376bdecc087
randyajackson <rajackson.dev@gmail.com>
2019-07-30 12:52:46 -0600

    7/30/19 1:00p

---
+++ finding greatest common divisor/findGCD.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int gcd(int x, int y)
+{
+
+    return ;
+}
+
+int main(void)
+{ 
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    int gcdResult;
+
+    gcdResult = gcd(x, y);
+    
+    return 0;
+}
